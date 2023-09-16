4e03fb06d66c9455c91abee9675b01b4427777d8
jmg <jmg41490400@gmail.com>
2023-09-16 22:35:55 +0900

    feat: Invitations

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -62,49 +62,39 @@ export const InviteModal = () => {
   };
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className={"bg-white text-black p-0 overflow-hidden"}>
-        <DialogHeader className={"pt-8 px-6"}>
-          <DialogTitle className={"text-2xl text-center font-bold"}>
-            Customize your server
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Invite Friends
           </DialogTitle>
-          <DialogDescription className={"text-center text-zinc-500"}>
-            Give your server a personality with a name and an image. Tou can
-            always change it later.
-          </DialogDescription>
         </DialogHeader>
-        <div className={"p-6"}>
-          <Label
-            className={
-              "uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
-            }
-          >
-            Server invte link
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
           </Label>
-          <div className={"flex items-center mt-2 gap-x-2"}>
+          <div className="flex items-center mt-2 gap-x-2">
             <Input
               disabled={isLoading}
-              className={
-                "bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-              }
+              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
               value={inviteUrl}
             />
-            <Button disabled={isLoading} size={"icon"} onClick={onCopy}>
+            <Button disabled={isLoading} onClick={onCopy} size="icon">
               {copied ? (
-                <Check className={"w-4 h-4"} />
+                <Check className="w-4 h-4" />
               ) : (
-                <Copy className={"w-4 h-4"} />
+                <Copy className="w-4 h-4" />
               )}
             </Button>
           </div>
           <Button
             onClick={onNew}
             disabled={isLoading}
-            variant={"link"}
-            size={"sm"}
-            className={"text-xs text-zinc-500 mt-4"}
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
           >
             Generate a new link
-            <RefreshCw className={"w-4 h-4 ml-2"} />
+            <RefreshCw className="w-4 h-4 ml-2" />
           </Button>
         </div>
       </DialogContent>
