e9450cf4e4b9b1b15da2f06e663681a2521474b3
jmg <jmg41490400@gmail.com>
2023-09-18 22:51:45 +0900

    feat: Chat Scroll hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -64,7 +64,7 @@ const ChatMessages = ({
     bottomRef,
     loadMore: fetchNextPage,
     shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
-    count: data?.pages?.[0].items.length ?? 0,
+    count: data?.pages?.[0]?.items?.length ?? 0,
   });
 
   if (status === "loading") {
--- hooks/use-chat-scroll.ts
+++ hooks/use-chat-scroll.ts
@@ -15,36 +15,44 @@ export const useChatScroll = ({
   loadMore,
   count,
 }: ChatScrollProps) => {
-  const [hasinitialized, setHasInitialized] = useState(false);
+  const [hasInitialized, setHasInitialized] = useState(false);
+
   useEffect(() => {
     const topDiv = chatRef?.current;
+
     const handleScroll = () => {
       const scrollTop = topDiv?.scrollTop;
+
       if (scrollTop === 0 && shouldLoadMore) {
         loadMore();
       }
     };
+
     topDiv?.addEventListener("scroll", handleScroll);
+
     return () => {
       topDiv?.removeEventListener("scroll", handleScroll);
     };
-  }, [chatRef, loadMore, shouldLoadMore]);
+  }, [shouldLoadMore, loadMore, chatRef]);
 
   useEffect(() => {
     const bottomDiv = bottomRef?.current;
     const topDiv = chatRef.current;
     const shouldAutoScroll = () => {
-      if (!hasinitialized && bottomDiv) {
+      if (!hasInitialized && bottomDiv) {
         setHasInitialized(true);
         return true;
       }
+
       if (!topDiv) {
         return false;
       }
+
       const distanceFromBottom =
-        topDiv?.scrollHeight - topDiv?.scrollTop - topDiv?.clientHeight;
+        topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
       return distanceFromBottom <= 100;
     };
+
     if (shouldAutoScroll()) {
       setTimeout(() => {
         bottomRef.current?.scrollIntoView({
@@ -52,5 +60,5 @@ export const useChatScroll = ({
         });
       }, 100);
     }
-  }, [bottomRef, chatRef, hasinitialized]);
+  }, [bottomRef, chatRef, count, hasInitialized]);
 };
